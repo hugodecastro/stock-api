63515293c439362c538edf684c936c6df24a8082
Hugo Castro <hugodecastro63@gmail.com>
2024-11-09 12:57:11 -0300

    add stock model

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# stock-api
+# StockApi
+
 API web application that retrieves stock data from an external financial API and performs minor data scraping from the Marketwatch financial website.
---
+++ src/model/Stock.py
@@ -0,0 +1,23 @@
+from sqlalchemy import ARRAY, JSON, Column, DateTime, Integer, String
+from sqlalchemy.sql import func
+
+from src.database import db
+
+
+class Stock(db.Model):
+    __tablename__ = 'stock'
+
+    id = Column(Integer, primary_key=True)
+    status = Column(String(20), nullable=False)
+    purchased_amount = Column(Integer, nullable=False)
+    purchased_status = Column(String(20), nullable=False)
+    request_data = Column(DateTime(timezone=True), server_default=func.now())
+    company_code = Column(String(100), nullable=False)
+    company_name = Column(String(100), nullable=False)
+    stock_values = Column(JSON, nullable=False)
+    performance_data = Column(JSON, nullable=False)
+    competitors = Column(ARRAY, nullable=False)
+    market_cap = Column(JSON, nullable=False)
+
+    def __repr__(self):
+        return f'<Stock {self.id}> has status {self.status}'

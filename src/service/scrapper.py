34a613e7ced36fd4d90639b6fb725931c1f49e01
Hugo Castro <hugodecastro63@gmail.com>
2024-11-12 10:57:33 -0300

    add spider

---
+++ src/service/scrapper.py
@@ -0,0 +1,20 @@
+import scrapy
+
+
+class StockScraper(scrapy.Spider):
+    name: str = "stocks"
+    allowed_domains = ["marketwatch.com"]
+
+    def start_requests(self):
+        headers = {
+            'Accept': ['text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'],
+            "Accept-Language": "en-US,en;q=0.9",
+            "Authorization": "",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        }
+
+        stock_url: str = "https://www.marketwatch.com/investing/stock/aapl"
+        yield scrapy.Request(url=stock_url, headers=headers, callback=self.parse)
+
+    def parse(self, response):
+        page = response

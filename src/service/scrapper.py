1ba2d8e31ab0e843548d997776a22c7e992ed490
Hugo Castro <hugodecastro63@gmail.com>
2024-11-25 16:02:20 -0300

    add beautiful soup

--- src/service/scrapper.py
+++ src/service/scrapper.py
@@ -1,4 +1,5 @@
 import scrapy
+from bs4 import BeautifulSoup
 
 
 class StockScraper(scrapy.Spider):
@@ -9,7 +10,6 @@ class StockScraper(scrapy.Spider):
         headers = {
             'Accept': ['text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'],
             "Accept-Language": "en-US,en;q=0.9",
-            "Authorization": "",
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
         }
 
@@ -17,4 +17,6 @@ class StockScraper(scrapy.Spider):
         yield scrapy.Request(url=stock_url, headers=headers, callback=self.parse)
 
     def parse(self, response):
-        page = response
+        soup = BeautifulSoup(response.body)
+
+        print(soup)
